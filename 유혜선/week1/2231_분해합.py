20a7ac39672422a1beb26427d0a127f587920612
Hye-S <gptjs7970@naver.com>
2022-08-03 01:50:03 +0900

    [220803] Add: 2231, 2798

---
+++ 유혜선/week1/2231_분해합.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    """
+    idea > 
+    1) i를 분해합 하였을때 n이 나오려면 i <= n 이어야함 
+    2) 문자열을 list() 의 인자로 넣을 경우 한 문자씩 분리된 리스트를 받을 수 있음
+    3) 숫자 -> 문자열 -> 리스트 로 각 자리수 합을 구하기
+    """
+    n = int(input())
+
+    for i in range(1, n):
+        if n == sum(list(map(int, list(str(i))))) + i:
+            return i
+        
+    return 0
+
+print(solution())
