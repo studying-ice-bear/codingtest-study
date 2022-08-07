708de112d427699213f9d1719ff84335cdcd11cf
elizabethgim <yujingim@kookmin.ac.kr>
2022-08-07 20:14:05 +0900

    백준-11729-하노이 탑 이동 순서

--- 김유진/Recursion/bj11729.py
+++ 김유진/Recursion/bj11729.py
@@ -3,18 +3,18 @@ def hanoi_num(n):
         return 1
     else:
         return 2 * hanoi_num(n-1) + 1
-def hanoi(start, end, n):
-    if(end == 1):
-        print(start, end)
-    # 빈 원반이 1, 2, 3 중 하나이고 서로 다르다.
-    # start + end + empty = 1+2+3 = 6
-    empty = 6-start-end
-    print(empty)
-    hanoi(start, empty, n-1)
-    print(start, end)
-    hanoi(empty, end, n-1)
+
+def hanoi(N, start, to, via):
+    if N == 1:
+        print(start, to)
+        return
+    else:
+        hanoi(N-1, start, via, to)
+        print(start, to)
+        hanoi(N-1, via, to, start)
 
 if __name__=="__main__":
     num = int(input())
-    print(2**num-1)
-    # hanoi(1, 3, num)
+    # print(2**num-1)
+    print(hanoi_num(num))
+    hanoi(num, 1, 3, 2)
