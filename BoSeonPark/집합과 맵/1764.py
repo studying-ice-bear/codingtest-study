72da2a4c459e7b3861b8b589a69094729a0f5c69
BoSeon Park <bedurgi999@gmail.com>
2022-08-13 09:35:41 +0900

    백준 1764 10816 풀이

--- BoSeonPark/집합과 맵/1764.py
+++ BoSeonPark/집합과 맵/1764.py
@@ -28,5 +28,5 @@ m개는 list로 저장
 list에 저장된 이름을 반복문을 통해 하나씩 가져와 key로 사용해 dictionary에
 해당 key가 있는지 확인
 존재하면 answer에 추가, 없으면 continue
-마지막에 answer list를 한 번 sort해주고 출력
+마지막에 answer list를 한 번 sort해주고 길이와 값들 출력
 """
