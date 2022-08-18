2ce0dd46b62d69d8bb559afb3a2e4101b1f28892
BoSeon Park <bedurgi999@gmail.com>
2022-08-18 14:27:56 +0900

    백준 백트래킹 15649 풀이

---
+++ BoSeonPark/백트래킹/15649.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split(' '))
+answer = []
+temp = []
+def dfs():
+    if len(temp) == m:
+        for num in temp:
+            print(num, end=' ')
+        print('')
+
+    for i in range(1, n+1):
+        if i not in temp:
+            temp.append(i)
+            dfs()
+            temp.pop()
+
+dfs()
