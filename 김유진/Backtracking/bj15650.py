854cf455c4be88399f06f19b0fdd194ce59fd6b1
elizabethgim <yujingim@kookmin.ac.kr>
2022-08-21 19:22:29 +0900

    다른 풀이

--- 김유진/Backtracking/bj15650.py
+++ 김유진/Backtracking/bj15650.py
@@ -6,3 +6,20 @@ for c in combinations:
     for i in c:
         print(i, end=' ')
     print()
+
+'''
+def back_tracking():
+    if len(stack) == M:
+        print(' '.join(list(map(str, stack))))
+    else:
+        for num in range(1, N + 1):
+            if num not in stack:
+                stack.append(num)
+                back_tracking()
+                stack.pop()
+
+
+N, M = map(int, input().split())
+stack = []
+back_tracking()
+'''
