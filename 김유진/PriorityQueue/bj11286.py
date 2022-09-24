5cf439d4d57d6dfd2f3537787748dee2784161aa
elizabethgim <yujingim@kookmin.ac.kr>
2022-09-24 20:22:11 +0900

    -

--- 김유진/PriorityQueue/bj11286.py
+++ 김유진/PriorityQueue/bj11286.py
@@ -13,4 +13,22 @@ for _ in range(N):
         if not absheap:
             print(0)
         else:
-            print(heapq.heappop(absheap)
+            print(heapq.heappop(absheap))
+
+'''
+import sys
+import heapq
+
+numbers = int(input())
+heap = []
+
+for _ in range(numbers):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(heap, (abs(num), num))
+    else:
+        try:
+            print(heapq.heappop(heap)[1])
+        except:
+            print(0)
+'''
