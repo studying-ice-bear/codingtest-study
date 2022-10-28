aea235e9eb81e5768174beb5d7081ce73b45e231
elizabethgim <yujingim@kookmin.ac.kr>
2022-10-28 23:59:57 +0900

    백준-12852-1로 만들기2, 실패

--- 김유진/week12/DynamicProgramming_BacktrackingShortestPath/bj12852.py
+++ 김유진/week12/DynamicProgramming_BacktrackingShortestPath/bj12852.py
@@ -2,3 +2,24 @@ import sys
 N = int(sys.stdin.readline())
 answer = 0
 dp = []
+
+for i in range(1, N+1):
+    dp.append(i)
+
+for i in range(2, N+1):
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3])
+    if N % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2])
+    dp[i] = min(dp[i], dp[i - 1] + 1)
+
+print(dp[N]-1)
+
+while N > 1:
+    print(N, end="")
+    if dp[N] == dp[N-1]+1:
+        N = N-1
+    elif N % 2 == 0 and dp[N] == (dp[N//2] + 1):
+        N = N // 2
+    elif N % 3 == 0 and dp[N] == (dp[N//3] +1):
+        N = N // 3
