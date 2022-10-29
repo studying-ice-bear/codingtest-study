036b244ec2bf40b544ae23a49b5f9fcdae983ea6
elizabethgim <yujingim@kookmin.ac.kr>
2022-10-29 21:43:40 +0900

    백준-12852-1로 만들기2

--- 김유진/week12/DynamicProgramming_BacktrackingShortestPath/bj12852.py
+++ 김유진/week12/DynamicProgramming_BacktrackingShortestPath/bj12852.py
@@ -1,8 +1,25 @@
 import sys
 N = int(sys.stdin.readline())
 answer = 0
-dp = []
+dp = [[0, []] for _ in range(N+1)]
 
+dp[1][0] = 0
+dp[1][1] = [1]
+
+for i in range(2, N+1):
+    dp[i][0] = dp[i-1][0] + 1
+    dp[i][1] = dp[i-1][1] + [i]
+    if i % 3 == 0 and dp[i//3][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i//3][0] + 1
+        dp[i][1] = dp[i // 3][1] + [i]
+    if i % 2 == 0 and dp[i//2][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i // 2][0] + 1
+        dp[i][1] = dp[i // 2][1] + [i]
+
+print(dp[N][0])
+print(*reversed(dp[N][1]))
+
+'''
 for i in range(1, N+1):
     dp.append(i)
 
@@ -23,3 +40,4 @@ while N > 1:
         N = N // 2
     elif N % 3 == 0 and dp[N] == (dp[N//3] +1):
         N = N // 3
+'''
