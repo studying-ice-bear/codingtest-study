71525a3861da23769e54a05f6bf3d6e15a1d3b43
Fibo4487 <fibonacci2246@gmail.com>
2022-12-12 01:13:34 +0900

    11047 add prev refactor

--- 김하영/Barkingdog_Greedy/11047.js
+++ 김하영/Barkingdog_Greedy/11047.js
@@ -19,3 +19,38 @@ const solution = () => {
 };
 
 console.log(solution());
+
+/* prev refactor
+const [N, K] = input.shift().split(" ").map(Number);
+
+const solution = () => {
+  const coins = input.map(Number).sort((a, b) => b - a);
+  let index = 0;
+  let result = 0;
+  let remain = K;
+
+  while (index !== N) {
+    const selectCoin = coins[index];
+    let value = remain - selectCoin;
+    if (value < 0) {
+      index++;
+      continue;
+    } else if (value > 0) {
+      let selectCoinEA = 4;
+      while (remain - selectCoin * selectCoinEA < 0) {
+        selectCoinEA--;
+      }
+      result += selectCoinEA;
+      remain -= selectCoin * selectCoinEA;
+      if (remain === 0) {
+        break;
+      }
+    } else {
+      result++;
+      break
+    }
+  }
+
+  return result;
+};
+*/
