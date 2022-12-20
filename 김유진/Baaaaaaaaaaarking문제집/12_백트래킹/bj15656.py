e9d84a5601a07729d80c9fb6d76e4464ea53683b
elizabethgim <yujingim@kookmin.ac.kr>
2022-12-20 21:55:16 +0900

    백준-15656-N과 M (7) 다른 풀이

--- 김유진/Baaaaaaaaaaarking문제집/12_백트래킹/bj15656.py
+++ 김유진/Baaaaaaaaaaarking문제집/12_백트래킹/bj15656.py
@@ -7,3 +7,18 @@ arr.sort()
 
 for a in map(list, product(arr, repeat = M)):
     print(*a, sep=' ')
+
+'''
+ans = []
+def dfs(depth, n, m):
+    if depth == m:
+        print(*ans)
+        return
+    for i in range(n):
+        ans.append(arr[i])
+        dfs(depth+1, n, m)
+        ans.pop()
+
+dfs(0, N, M)
+
+'''
