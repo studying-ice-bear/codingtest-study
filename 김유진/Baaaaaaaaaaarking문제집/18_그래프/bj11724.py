cd93bd12a1104e384311e01ff3e253eaaa529f87
elizabethgim <yujingim@kookmin.ac.kr>
2023-02-01 21:17:47 +0900

    백준-11724-연결 요소의 개수, bfs 풀이

--- 김유진/Baaaaaaaaaaarking문제집/18_그래프/bj11724.py
+++ 김유진/Baaaaaaaaaaarking문제집/18_그래프/bj11724.py
@@ -1,5 +1,6 @@
 import sys
-sys.setrecursionlimit(5000)
+# sys.setrecursionlimit(5000)
+from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
@@ -16,6 +17,16 @@ def dfs(start, depth):
         if not visited[i]:
             dfs(i, depth + 1)
 
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        node = queue.popleft()
+        for i in graph[node]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
 visited = [False] * (N+1)
 count = 0
 
@@ -25,7 +36,8 @@ for i in range(1, N+1):
             count += 1
             visited[i] = True
         else:
-            dfs(i, 0)
+            # dfs(i, 0)
+            bfs(i)
             count += 1
 
 print(count)
