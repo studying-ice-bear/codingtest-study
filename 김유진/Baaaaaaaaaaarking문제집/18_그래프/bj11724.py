39cec7111b70b31df38ce9ab2d877a32f1f1fed3
elizabethgim <yujingim@kookmin.ac.kr>
2023-02-01 21:14:21 +0900

    백준-11724-연결 요소의 개수, dfs 풀이

--- 김유진/Baaaaaaaaaaarking문제집/18_그래프/bj11724.py
+++ 김유진/Baaaaaaaaaaarking문제집/18_그래프/bj11724.py
@@ -1,4 +1,6 @@
 import sys
+sys.setrecursionlimit(5000)
+
 N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
 
@@ -7,3 +9,27 @@ for _ in range(M):
     graph[x].append(y)
     graph[y].append(x)
 
+def dfs(start, depth):
+    visited[start] = True
+
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i, depth + 1)
+
+visited = [False] * (N+1)
+count = 0
+
+for i in range(1, N+1):
+    if not visited[i]:
+        if not graph[i]:
+            count += 1
+            visited[i] = True
+        else:
+            dfs(i, 0)
+            count += 1
+
+print(count)
+
+'''
+https://ji-gwang.tistory.com/292
+'''
