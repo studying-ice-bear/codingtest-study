869e157018aa974eb79e0524f3bb439a65e13cf8
elizabethgim <yujingim@kookmin.ac.kr>
2023-02-13 16:13:40 +0900

    백준-14888-연산자 끼워넣기, 백트랙킹(dfs)

--- 김유진/Baaaaaaaaaaarking문제집/13_시뮬레이션/bj14888.py
+++ 김유진/Baaaaaaaaaaarking문제집/13_시뮬레이션/bj14888.py
@@ -27,7 +27,30 @@ output:
 '''
 
 import sys
+
 N = int(sys.stdin.readline())
 numbers = list(map(int, sys.stdin.readline().split()))
-add, sub, mul, div = map(int, sys.stdin.readline().split())
+op = list(map(int, sys.stdin.readline().split()))
 
+maximum = -1e9
+minimum = 1e9
+
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+
+    if plus:
+        dfs(depth+1, total+numbers[depth], plus-1, minus, multiply, divide)
+    if minus:
+        dfs(depth+1, total-numbers[depth], plus, minus-1, multiply, divide)
+    if multiply:
+        dfs(depth+1, total*numbers[depth], plus, minus, multiply-1, divide)
+    if divide:
+        dfs(depth+1, int(total/numbers[depth]), plus, minus, multiply, divide-1)
+
+dfs(1, numbers[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
