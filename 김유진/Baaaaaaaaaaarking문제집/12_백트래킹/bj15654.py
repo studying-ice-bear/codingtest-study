428a2c1d8f4a2ab822bce9bee6ed18370d231e79
elizabethgim <yujingim@naver.com>
2023-02-28 20:37:24 +0900

    백준-15654-N과 M(1), 다른 풀이 - 재귀 활용

--- 김유진/Baaaaaaaaaaarking문제집/12_백트래킹/bj15654.py
+++ 김유진/Baaaaaaaaaaarking문제집/12_백트래킹/bj15654.py
@@ -1,8 +1,30 @@
 import sys
-from itertools import permutations
 
 N, M = map(int, sys.stdin.readline().split())
-arr = list(map(int, sys.stdin.readline().split()))
+arr = [i for i in range(0, 9)]
+
+isused = [False] * N
+
+def func(k):
+    if k == M:
+        for i in range(M):
+            print(arr[i], end=' ');
+        print()
+        return
+
+    for i in range(N):
+        if not isused[i]:
+            arr[k] = i
+            isused[i] = True
+            func(k+1)
+            isused[i] = False
+
+func(0);
+
+'''
+import sys
+from itertools import permutations
 arr.sort()
 for a in map(list, permutations(arr, M)):
     print(*a, sep=' ')
+'''
