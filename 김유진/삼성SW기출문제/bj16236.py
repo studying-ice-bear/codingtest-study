6dcb1c87d0a12384c6cadaf2c4f1fdca2b9c0f3a
elizabethgim <yujingim@naver.com>
2023-04-05 21:04:55 +0900

    백준-16236-아기 상어, testcase만 통과

--- 김유진/삼성SW기출문제/bj16236.py
+++ 김유진/삼성SW기출문제/bj16236.py
@@ -1,31 +1,37 @@
 from collections import deque
+
 N = int(input())
 
 sea = []
 
 for _ in range(N):
-    sea.append(list(map(int, input().split(' '))))
+    sea.append(list(map(int, input().split())))
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-# 아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없고,
-def dfs(q, to_x, to_y, total, visited):
+def countMove(graph, abletoMove, startpoint):
+    moveGraph = [[0] * N for _ in range(N)]
+    visited = [[False]*N for _ in range(N)]
 
-    while q:
-        x, y = q.popleft()
-        if to_x == x and to_y == y:
-            return total
-        total += 1
+    a, b = startpoint
+    que = deque()
+    que.append((a, b))
+    visited[a][b] = True
+
+    while que:
+        x, y = que.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+            if 0 <= nx < N and 0 <= ny < N and abletoMove[nx][ny] and not visited[nx][ny]:
+                moveGraph[nx][ny] = moveGraph[x][y] + 1
+                que.append((nx, ny))
                 visited[nx][ny] = True
-                dfs(q, nx, ny, total, visited)
 
+    return moveGraph
 
 
 babyshark = 2
@@ -44,20 +50,28 @@ while True:
                 sea[i][j] = 0
                 go[i][j] = True
             elif sea[i][j] != 0 and sea[i][j] < babyshark:
-                que.append((i, j, sea[i][j]))
+                que.append((i, j))
                 go[i][j] = True
-            elif sea[i][j] == 0:
+            elif sea[i][j] == 0 or sea[i][j] <= babyshark:
                 go[i][j] = True
 
     # 더 이상 먹을 수 있는 물고기가 공간에 없다면 아기 상어는 엄마 상어에게 도움을 요청한다.
     if not que:
         break
 
+    moveGraph = countMove(sea, go, start)
     x, y = start
+
+    # print(time)
+    # print("shark: ", babyshark, eaten)
+    # print(x, y)
+    # print(*sea, sep='\n')
+    # print()
+    # print(*moveGraph, sep='\n')
     # 먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다.
     if len(que) == 1:
-        a, b, c = que.pop()
-        time += abs(x-a) + abs(y-b)
+        a, b = que.pop()
+        time += moveGraph[a][b]
 
         start = (a, b)
         sea[a][b] = 9
@@ -67,37 +81,35 @@ while True:
     else:
         # 먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
         # 거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 지나야하는 칸의 개수의 최솟값이다.
-        canEat = 400
-        x, y = start
+        canEat = 402
         to = start
 
-        for i in range(len(que)):
-            a, b, c = que[i]
-            total = abs(x-a) + abs(y-b)
+        while que:
+            qx, qy = que.popleft()
+            distance = moveGraph[qx][qy]
 
-            # 거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 지나야하는 칸의 개수의 최솟값이다.
-            if total < canEat:
-                canEat = total
-                to = (a, b, c)
-            # 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
-            elif total == canEat:
-                if to[0] > a:
-                    to = (a, b, c)
-                elif to[0] == a:
-                    if to[1] > b:
-                        to = (a, b, c)
-        
-        
-        sea[x][y] = 0
+            if distance == 0:
+                to = start
+            else:
+                if canEat > distance:
+                    canEat = distance
+                    to = (qx, qy)
+
+                elif canEat == distance:
+                    if to[0] > qx:
+                        to = (qx, qy)
+                    elif to[0] == qx:
+                        if to[1] > qy:
+                            to = (qx, qy)
+
+
+        time += moveGraph[to[0]][to[1]]
         sea[to[0]][to[1]] = 9
-
-        time += canEat
         eaten += 1
-        '''
-        total = dfs(que, start[0], start[1], 0, go)
-        print(total)
-        time += total
-        '''
+
+        if to == start:
+            break
+
     if eaten == babyshark:
         babyshark += 1
         eaten = 0
@@ -105,6 +117,68 @@ while True:
 print(time)
 
 '''
+5
+0 0 5 0 0
+0 6 1 7 0
+8 2 0 3 10
+0 9 4 11 0
+0 0 12 0 0
+
+3
+0 1 1
+4 4 4
+0 9 0
+
+5
+1 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 3
+0 0 0 3 9
+
+5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+9 2 3 4 5
+1 2 3 4 5
+
+10
+9 2 0 0 0 0 0 0 0 1 
+0 3 0 0 0 0 0 0 0 0
+3 3 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+
+20
+9 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+3 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+
+
 3
 0 0 0
 0 0 0
@@ -115,5 +189,41 @@ print(time)
 [2, 3] [2, 2] [2, 3]
 [2, 2] [2, 1] [2, 2]
 [2, 1] [2, 0] [2, 1]
+
+----------------------------
+agraph =[
+    [4, 3, 2, 1],
+    [0, 0, 0, 0],
+    [0, 0, 9, 0],
+    [1, 2, 3, 4]
+]
+
+aMove = [
+    [False, False, False, True],
+    [True, True, True, True],
+    [True, True, True, True],
+    [True, False, False, False]
+]
+
+N = 4
+
+start = (2, 2)
+------------------------------------
+
+# 아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없고,
+def dfs(x, y, visited, a, b, total):
+    visited[x][y] = True
+
+    if x == a and y == b:
+        return total
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+            visited[nx][ny] = True
+            total = dfs(nx, ny, visited, a, b, total+1)
+            visited[nx][ny] = False
 
 '''
